2f6a475c2eae8c17466c2872f4e45a5cbe00783b
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2016-08-15 21:32:24 -0600

    Add MBE Ventures project data

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,10 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
-
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
 
 // Default task
 gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
@@ -24,7 +14,6 @@ gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 gulp.task('less', function() {
     return gulp.src('less/grayscale.less')
         .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
@@ -46,7 +35,6 @@ gulp.task('minify-css', function() {
 gulp.task('minify-js', function() {
     return gulp.src('js/grayscale.js')
         .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
